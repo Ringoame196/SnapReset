05da1dc2d6583e74862d4d9dbbcaae88a375071f
りんご飴196 <kazumac41@gmail.com>
2025-05-29 16:16:33 +0900

    ✨ reset追加

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "com.github.ringoame196"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(22)
+}
+tasks.register<Copy>("reset") {
+    val srcDirPath = "src/main/java/"
+    val srcDir = projectDir.resolve(srcDirPath).apply(File::mkdirs)
+    srcDir.resolve("Main.java").writeText(
+        """
+            public class Main {
+                public static void main(String[] args) {
+
+                }
+            }
+            """.trimIndent()
+    )
+}
