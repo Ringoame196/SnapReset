f81479559ed02e24336319b4e13dfdd35b76ba4e
りんご飴196 <kazumac41@gmail.com>
2025-05-30 20:57:00 +0900

    ⬆️ スキャナーをリセット時に記載するように

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,9 +24,12 @@ tasks.register<Copy>("reset") {
     val srcDir = projectDir.resolve(srcDirPath).apply(File::mkdirs)
     srcDir.resolve("Main.java").writeText(
         """
+            import java.util.*;
+            
             public class Main {
                 public static void main(String[] args) {
-
+                    Scanner sc = new Scanner(System.in);
+                    System.out.println("Hello World");
                 }
             }
             """.trimIndent()
