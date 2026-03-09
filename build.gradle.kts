47e5c9639e25935fa4f1ba7c8df48bdd6da3e1ef
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2026-03-09 20:17:18 +0900

    ⬆️ reset処理を変更

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "com.github.ringoame196"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -18,33 +18,4 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(22)
-}
-tasks.register<Copy>("reset") {
-    val srcJavaDirPath = "src/main/java/"
-    val srcJavaDir = projectDir.resolve(srcJavaDirPath).apply(File::mkdirs)
-    srcJavaDir.resolve("Main.java").writeText(
-        """
-            import java.util.*;
-            
-            public class Main {
-                public static void main(String[] args) {
-                    Scanner sc = new Scanner(System.in);
-                    System.out.println("Hello World");
-                }
-            }
-        """.trimIndent()
-    )
-
-    val srcKotlinDirPath = "src/main/kotlin/"
-    val srcKotlinDir = projectDir.resolve(srcKotlinDirPath).apply(File::mkdirs)
-    srcKotlinDir.resolve("Main.kt").writeText(
-        """
-            import java.util.Scanner
-
-            fun main(args: Array<String>) {
-	            val scanner = Scanner(System.`in`)
-	            println("Hello World")
-            }
-        """.trimIndent()
-    )
 }
---
+++ reset.bat
@@ -0,0 +1,2 @@
+xcopy /Y .\Template\Main.java .\src\main\java\Main.java
+xcopy /Y .\Template\Main.kt .\src\main\kotlin\Main.kt
